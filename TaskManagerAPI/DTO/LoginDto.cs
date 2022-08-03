2c6211578deaa4ee55c9fe38d02c2224d947c63c
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-08-03 18:28:44 -0400

    Commit: Unit Test Project created Details: 1. Unit test project created. 2. Pending files commited.

---
+++ TaskManagerAPI/DTO/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerAPI.DTO
+{
+    public class LoginDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        public string Password { get; set; } = String.Empty;
+    }
+}
+
---
+++ TaskManagerAPI/DTO/RegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerAPI.DTO
+{
+    public class RegisterDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Must be of minimum 5 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = String.Empty;
+
+        [Required]
+        public string FullName { get; set; } = String.Empty;
+    }
+}
+
