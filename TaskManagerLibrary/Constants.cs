6400f1c05d496584ddb3b31a0af4140e9c1ada73
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-08-08 02:51:23 -0400

    Commits: Library project created. Details: 1. Library project created. 2. Nuget packages installed in the library project to make api requests. 3. Method created in library project to call sign up api request. 4. Library method for sign up called from sign up button in the core web application.

---
+++ TaskManagerLibrary/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TaskManagerLibrary
+{
+    public class Constants
+    {
+        public const string BaseUrl = "http://localhost:7038/";
+
+        public const string SignUpAPI = BaseUrl + "api/Employees/signup";
+        public const string SignInAPI = BaseUrl + "api/Employees/signin";
+        public static string Token = "";
+    }
+}
+
