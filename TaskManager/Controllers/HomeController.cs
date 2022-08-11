ea20ff5e44602b43e790d26028c59c7501c63b22
Moxit <shah.moxit999@gmail.com>
2022-08-11 04:13:00 -0400

    Added Project creation page
    Added Project page and Project list page.

--- TaskManager/Controllers/HomeController.cs
+++ TaskManager/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult CreateProject()
+    {
+        return View();
+    }
+
+    public IActionResult ProjectList()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
