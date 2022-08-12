4859723f1df635e4ad62d86c01106022958a59a5
Moxit <shah.moxit999@gmail.com>
2022-08-11 20:38:26 -0400

    Added tests
    Added tests for the user interface to look the title of the webpage matches when it initialise.
    Added tests for the project controller for project id lookup.

---
+++ TaskManagerTests/UserInterfaceTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Protocol.Core.Types;
+using TaskManagerAPI.Controllers;
+using TaskManagerAPI.Models;
+using TaskManger.Areas.Identity.Pages.Account;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using TaskManagerLibrary;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+
+namespace UserInterfaceTests;
+
+[TestClass]
+public class UserInterfaceTests
+{
+
+    IWebDriver _driver;
+
+    [TestInitialize]
+    public void Startup()
+    {
+        new DriverManager().SetUpDriver(new ChromeConfig());
+        _driver = new SafariDriver();
+    }
+
+
+    [TestMethod]
+    public void TestLoginTitle()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Login");
+        Assert.AreEqual("Log in", _driver.Title);
+        Assert.IsTrue(_driver.Title.Contains("Log in"));
+    }
+
+
+    [TestMethod]
+    public void TestRegisterTitle()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Register");
+        Assert.AreEqual("Regsiter", _driver.Title);
+        Assert.IsTrue(_driver.Title.Contains("Register"));
+    }
+
+
+}
